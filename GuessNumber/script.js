c20c8f2824e3b747e9e5201c5790017d68589225
Hamza Atakroury <44806256+Hamzaftak@users.noreply.github.com>
2022-12-01 09:34:09 +0100

    Add files via upload

---
+++ GuessNumber/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const secret = 10;
+let currentScore = 20;
+document.querySelector(".score").textContent = currentScore;
+let highScore = 0;
+document.querySelector(".highscore").textContent = highScore;
+document.querySelector(".check").addEventListener("click", function () {
+  let value = document.querySelector(".guess").value;
+  if (value > 20 || value < 1) {
+    document.querySelector(".message").textContent =
+      "😒 The number should be between 1 and 20 !!!";
+  } else {
+    if (value < secret || value > secret) {
+      if (currentScore > 0) {
+        if (value < secret) {
+          document.querySelector(".message").textContent = "👇 Too Low!";
+        } else {
+          document.querySelector(".message").textContent = "👆 Too High!";
+        }
+        currentScore = document.querySelector(".score").textContent;
+        currentScore--;
+        document.querySelector(".score").textContent = currentScore;
+        if (currentScore === 0) {
+          document.body.style.backgroundColor = "red";
+          document.querySelector(".message").textContent =
+            "😭 Game over, Try again!";
+        }
+      } else {
+        document.body.style.backgroundColor = "red";
+        document.querySelector(".message").textContent =
+          "😭 Game over, Try again!";
+      }
+    } else {
+      document.querySelector(".message").textContent = "Correct!";
+      document.body.style.backgroundColor = "green";
+      document.querySelector(".number").textContent = value;
+      document.querySelector(".check").disabled = true;
+
+      if (currentScore > highScore) {
+        highScore = currentScore;
+        document.querySelector(".highscore").textContent = highScore;
+      }
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  currentScore = 20;
+  document.querySelector(".score").textContent = currentScore;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".check").disabled = false;
+  document.body.style.backgroundColor = "black";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+});
